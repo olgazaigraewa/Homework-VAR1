a8ce2976f616b3cc86efef22ef46baf3b3933f73
olgazaigraewa <ol.zaigraewa@yandex.ru>
2022-10-02 00:05:56 +0300

    Переменные часть 1

---
+++ src/Main.java
@@ -0,0 +1,97 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        //Задание 1
+        System.out.println("Задача 1 ");
+        var dog = 8;
+        System.out.println(dog);
+        var cat = 3.6;
+        System.out.println(cat);
+        var paper = 763789;
+        System.out.println(paper);
+
+        //Задание 2
+        System.out.println("Задача 2 ");
+        dog = dog +4;
+        System.out.println(dog);
+        cat = cat +4;
+        System.out.println(cat);
+        paper = paper + 4;
+        System.out.println(paper);
+
+        //Задание 3
+        System.out.println("Задача 3 ");
+        var dog1 = 8.0;
+        dog1 = dog1 - 3.5;
+        System.out.println(dog1);
+        cat = cat -1.6;
+        System.out.println(cat);
+        paper = paper - 7639;
+        System.out.println(paper);
+
+        //Задание 4
+        System.out.println("Задача 4 ");
+        var friend = 19;
+        System.out.println(friend);
+        friend = friend + 2;
+        System.out.println(friend);
+        friend = friend / 7;
+        System.out.println(friend);
+
+        //Задание 5
+        System.out.println("Задача 5 ");
+        var frog = 3.5;
+        System.out.println(frog);
+        frog = frog * 10;
+        System.out.println(frog);
+        frog = frog / 3.5;
+        System.out.println(frog);
+        frog = frog + 4;
+        System.out.println(frog);
+
+        //Задание 6
+        System.out.println("Задача 6 ");
+        var boxer1 = 78.2;
+        var boxer2 = 82.7;
+        var weightOfAllBoxers = boxer1 + boxer2;
+        System.out.println("Общий вес боксеров " + weightOfAllBoxers );
+        var weightDifference = boxer2 - boxer1;
+        System.out.println("Разница в весе боксеров " + weightDifference );
+
+        //Задание 7
+        System.out.println("Задача 7 ");
+        var weightDifferenceOnWay = boxer2 - boxer1;
+        System.out.println("Разница в весе боксеров 1 способ " + weightDifferenceOnWay );
+        var weightDifferenceSecondWay = boxer2 % boxer1;
+        System.out.println("Разница в весе боксеров 2 способ " + weightDifferenceSecondWay );
+
+        //Задание 8
+        System.out.println("Задача 8 ");
+        var allWorkingTimes = 640;
+        var workingTimesOfOnePersons = 8;
+        var totalNamberOfPersons =allWorkingTimes / workingTimesOfOnePersons;
+        System.out.println("Всего сотрудников в компании " + totalNamberOfPersons + " человек ");
+        totalNamberOfPersons = totalNamberOfPersons + 94;
+        System.out.println("Всего сотрудников в компании " + totalNamberOfPersons + " человека ");
+        var totalWorkingTimes =  totalNamberOfPersons * workingTimesOfOnePersons;
+        System.out.println("Если всего сотрудников в компании " + totalNamberOfPersons + " человека " + "Всего рабочих часов потребуется " + totalWorkingTimes );
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+   }
+
+
+
+
